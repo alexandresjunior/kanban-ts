c0886ebd0a089678f73226d8e6bec34845362adc
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-01-16 21:20:38 -0300

    Criação da tela Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { HomeComponent } from "./paginas/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
