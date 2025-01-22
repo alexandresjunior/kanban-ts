534a315f513efdd5d872fb95fe2278725127aab1
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-01-21 21:19:54 -0300

    Definição de rotas; instalação do json-server

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { HomeComponent } from './paginas/home/home.component';
 export const routes: Routes = [
     {
         path: '',
+        title: 'Kanban | Login',
         component: LoginComponent
     },
     {
         path: 'home',
+        title: 'Kanban | Home',
         component: HomeComponent
     }
 ];
--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  router: Router = new Router;
+
+  usuario = {
+    email: "",
+    senha: "",
+    manterConectado: true
+  }
+
+  fazerLogin(): void {
+    // Chamada a API de autenticação
+    console.log(this.usuario);
+
+    if (this.usuario.manterConectado) {
+      localStorage.setItem('usuario_kanban', JSON.stringify(this.usuario));
+    }
+
+    sessionStorage.setItem('usuario_kanban', JSON.stringify(this.usuario));
+
+    this.router.navigate(['home']);
+  }
 
 }
