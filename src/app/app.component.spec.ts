0e795cb43ea6469ebd1623ff2c010ae294940eb6
Alexandre de Souza Jr. <alexandrejr.dev@gmail.com>
2025-01-27 08:36:35 -0300

    Atualização do nome do projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'kanban-ts' title`, () => {
+  it(`should have the 'kanban-redmine-ts' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kanban-ts');
+    expect(app.title).toEqual('kanban-redmine-ts');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kanban-ts');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kanban-redmine-ts');
   });
 });
