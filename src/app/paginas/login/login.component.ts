f8c6fadcc0e0e2a4a16fe9a31b3fb2932114f34e
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-02-07 20:18:34 -0300

    Validação da tela de login

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,29 +1,46 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AutenticacaoService } from '../../servicos/autenticacao.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  erro: string = "";
   usuario = {
     email: "",
     senha: "",
     manterConectado: true
   }
 
-  erro: string = "";
-
   constructor(
     private autenticacaoService: AutenticacaoService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{6,}$/)
+      ]],
+      manterConectado: true
+    })
+  }
 
   fazerLogin(): void {
+    if (this.loginForm.invalid) {
+      this.erro = "Por favor, preencha os campos corretamente.";
+      return;
+    }
+
     this.autenticacaoService.fazerLogin(this.usuario.email, this.usuario.senha).subscribe(resposta => {
       if (this.usuario.manterConectado) {
         localStorage.setItem('usuario_kanban', JSON.stringify(resposta));
