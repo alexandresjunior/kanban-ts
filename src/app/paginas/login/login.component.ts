625615a0ff04fe5a0cbf143cde56b00b43067d18
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-02-07 18:55:29 -0300

    Validação dos campos da tela de login

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,40 +1,51 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AutenticacaoService } from '../../servicos/autenticacao.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  usuario = {
-    email: "",
-    senha: "",
-    manterConectado: true
-  }
-
+  loginForm: FormGroup;
   erro: string = "";
 
   constructor(
     private autenticacaoService: AutenticacaoService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/)
+      ]],
+      manterConectado: [true]
+    });
+  }
 
   fazerLogin(): void {
-    this.autenticacaoService.fazerLogin(this.usuario.email, this.usuario.senha).subscribe(resposta => {
-      if (this.usuario.manterConectado) {
+    if (this.loginForm.invalid) {
+      this.erro = "Por favor, preencha os campos corretamente.";
+      return;
+    }
+
+    const { email, senha, manterConectado } = this.loginForm.value;
+    this.autenticacaoService.fazerLogin(email, senha).subscribe(resposta => {
+      if (manterConectado) {
         localStorage.setItem('usuario_kanban', JSON.stringify(resposta));
       }
-
       sessionStorage.setItem('usuario_kanban', JSON.stringify(resposta));
-
       this.router.navigate(['home']);
     }, erro => {
       this.erro = erro.message;
-    })
+    });
   }
-
 }
