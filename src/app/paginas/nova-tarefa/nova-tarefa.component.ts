212bfa3f65d4ce2e4d75d36bd9c7f4c7da6860d8
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-02-07 21:01:59 -0300

    Validação da tela de nova tarefa

--- src/app/paginas/nova-tarefa/nova-tarefa.component.ts
+++ src/app/paginas/nova-tarefa/nova-tarefa.component.ts
@@ -9,18 +9,26 @@ import { Status } from '../../interfaces/status';
 import { TiposTarefaService } from '../../servicos/tipos-tarefa.service';
 import { StatusesService } from '../../servicos/statuses.service';
 import { Tarefa } from '../../interfaces/tarefa';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TarefasService } from '../../servicos/tarefas.service';
 import { ModalComponent } from "../../componentes/modal/modal.component";
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-tarefa',
-  imports: [CabecalhoComponent, RodapeComponent, CommonModule, FormsModule, ModalComponent],
+  imports: [
+    CabecalhoComponent,
+    RodapeComponent,
+    CommonModule,
+    FormsModule,
+    ModalComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './nova-tarefa.component.html',
   styleUrl: './nova-tarefa.component.css'
 })
 export class NovaTarefaComponent {
+  novaTarefaForm: FormGroup;
   usuarios: Usuario[] = [];
   tiposTarefa: TipoTarefa[] = [];
   statuses: Status[] = [];
@@ -43,8 +51,15 @@ export class NovaTarefaComponent {
     private statusesService: StatusesService,
     private tarefasService: TarefasService,
     private router: Router,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {
+    this.novaTarefaForm = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(5)]],
+      autor: ['', [Validators.required]],
+      tipoTarefa: ['', [Validators.required]],
+    })
+  }
 
   ngOnInit(): void {
     this.usuariosService.listarUsuarios().subscribe((usuarios) => {
@@ -70,6 +85,11 @@ export class NovaTarefaComponent {
   }
 
   salvarTarefa(): void {
+    if (this.novaTarefaForm.invalid) {
+      alert("Preencha os campos obrigatórios.");
+      return;
+    }
+
     if (this.novaTarefa.id !== 0) {
       // Se 'id' é válido/existente, atualizar a tarefa.
       this.tarefasService.atualizarTarefa(this.novaTarefa.id, this.novaTarefa).subscribe(resposta => {
